94acf1db679f60bd85cd56c937d3008ff70a6e41
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2025-06-20 22:16:25 +0300

    Remove Customer, Order, OrderItem, ShopItem, ShopItemCategory models, related controllers, and test cases; clean up EComDbContext

--- EComApi/Program.cs
+++ EComApi/Program.cs
@@ -1,19 +1,11 @@
-using Microsoft.EntityFrameworkCore;
-using EComApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-        options.JsonSerializerOptions.WriteIndented = true;
-    });
-builder.Services.AddDbContext<EComDbContext>(options =>
-    options.UseInMemoryDatabase("EComApiDb"));
+builder.Services.AddControllers();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -26,17 +18,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Map controllers
 app.MapControllers();
 
-// Initialize the database with seed data
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<EComDbContext>();
-    context.Database.EnsureCreated();
-}
-
 app.Run();
-
-// Make Program class accessible for testing
-public partial class Program { }
